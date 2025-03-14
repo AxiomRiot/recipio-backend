0d4013878f10e124c56c0e7798b7220ae7dcfe05
Zachery Knopp <zacheryknopp@gmail.com>
2025-03-14 16:14:13 -0400

    Added starting application log line

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const logger = require('./utils/loggers');
 
 const port = process.env.PORT;
 
+logger.info("Starting Application");
+
 app.listen(port, () => {
   logger.info(`Server is up on port ${port}`);
 });
